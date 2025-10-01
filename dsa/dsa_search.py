f9e3d969f621e274b8d472cf21789568b478e28d
N-Iregi <n.iregi@alustudent.com>
2025-10-01 09:14:59 +0000

    Create dsa_search.py

---
+++ dsa/dsa_search.py
@@ -0,0 +1,62 @@
+import json
+import time
+
+
+# load parsed json data from file and convert to python object
+with open("../data/parsed_sms.json", "r", encoding="utf-8") as file:
+    transactions = json.load(file)
+
+
+# use the transactions with IDs to enable easy search
+for i, t in enumerate(transactions, start=1):
+    t["id"] = i
+
+
+# Build Data Structures to use
+
+# a list of the transactions
+transactions_list = transactions
+# a dictionary of the transactions with an id for each transaction
+transactions_dictionary = {t["id"]: t for t in transactions}
+
+
+# implement linear search function
+def linear_search(transactions_list, spec_id):
+    for transaction in transactions_list:
+        # look up specified id and return the particular record
+        if transaction["id"] == spec_id:
+            return t
+    return None
+
+# implement dictionary lookup to find transaction by key
+
+
+def dictionary_lookup(transactions_dictionary, spec_id):
+    return transactions_dictionary.get(spec_id, None)
+
+
+# measure efficiency of the search algos using the last transaction
+spec_id = len(transactions)
+
+# Linear search time taken
+start_time = time.time()
+linear_result = linear_search(transactions_list, spec_id)
+end_time = time.time()
+linear_total_time = end_time - start_time
+print(f"Linear search result: {linear_result}\n")
+print(f"Our linear search took a time of {linear_total_time} seconds")
+
+print()
+
+# Dictionary lookup time taken
+start_time = time.time()
+dict_lookup_result = dictionary_lookup(transactions_dictionary, spec_id)
+end_time = time.time()
+dict_total_time = end_time - start_time
+print(f"Dictionary lookup result: {dict_lookup_result}\n")
+print(f"Our dictionary lookup took a time of {dict_total_time} seconds\n")
+
+if (linear_total_time > dict_total_time):
+	print("Dictionary lookup is faster than linear search")
+else:
+	print("linear search is faster than dictionary lookup")
